63f09ec4677f5d6b560904cef32215e93b4389f8
SandyRoxx <44232438+SandyRoxx@users.noreply.github.com>
2019-04-04 14:25:01 +0530

    Coding Blocks Lecture

---
+++ Lecture-5/script.js
@@ -0,0 +1,35 @@
+
+let x=10
+console.log(x)
+const y=20
+console.log(y)
+
+const obj={
+ a:10,
+ b:true,
+ c:'sandy'
+}
+console.log(obj)
+
+obj.a=20
+console.log(obj)
+delete obj.b
+console.log(obj)
+//00obj={d:20}
+console.log(obj)
+
+function sayHello(name){
+    console.log('Hello '+ name);
+}
+
+sayHello('sandy')
+let a
+console.log(parseInt(a))
+console.log(parseInt('11a'))
+
+var ob=document.getElementById('mydiv');
+
+console.log(ob)
+console.log(ob.innerHTML)
+console.log(ob.innerText)
+console.log(ob.textContent)
